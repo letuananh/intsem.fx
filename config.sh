a8493ddbd8aba00965b42f6a925f087db725e2b9
Le Tuan Anh <tuananh.ke@gmail.com>
2014-12-02 20:56:40 +0700

    Update gitmodules

--- config.sh
+++ config.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/sh
 
-ln -s pydelphin/delphin delphin
+function link_folder {
+	FOLDER_PATH=$1
+	SYMLINK_NAME=$2
+	if [ ! -d ${SYMLINK_NAME} ]; then
+		ln -sv ${FOLDER_PATH} ${SYMLINK_NAME}
+	else
+		echo "Folder ${SYMLINK_NAME} exists."
+	fi
+}
 
+function link_file {
+	FOLDER_PATH=$1
+	SYMLINK_NAME=$2
+	if [ ! -f ${SYMLINK_NAME} ]; then
+		ln -sv ${FOLDER_PATH} ${SYMLINK_NAME}
+	else
+		echo "File ${SYMLINK_NAME} exists."
+	fi
+}
+
+link_folder ~/workspace/pydelphin/delphin delphin
+link_folder ~/workspace/beautifulsoup/bs4-python3 bs4
+link_folder ~/workspace/nltk/nltk nltk
+link_file ~/workspace/erg/erg.dat data/erg.dat
