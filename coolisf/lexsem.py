a392b23adbbb964b289fb7ede238ef5d531905bb
Le Tuan Anh <tuananh.ke@gmail.com>
2018-08-05 13:54:45 +0800

    Update special chars

--- coolisf/lexsem.py
+++ coolisf/lexsem.py
@@ -43,7 +43,7 @@ from yawlib import SynsetID
 # CONFIGURATION
 # -------------------------------------------------------------------------------
 
-SPECIAL_CHARS = '''?!"'$-_&|.,;:'''
+SPECIAL_CHARS = '''?!"'$-_&|.,;:“” '''
 # TODO: Move this to chirptext
 PREPS = ['aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'amid', 'among', 'anti', 'around', 'as', 'at', 'before', 'behind', 'below', 'beneath', 'beside', 'besides', 'between', 'beyond', 'but', 'by', 'concerning', 'considering', 'despite', 'down', 'during', 'except', 'excepting', 'excluding', 'following', 'for', 'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'of', 'off', 'on', 'onto', 'opposite', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'than', 'through', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'versus', 'via', 'with', 'within', 'without', 'that']
 PREPS_PLUS = PREPS + ['a']
