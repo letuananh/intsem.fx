91613b0f2c5d393312b5bae60e71fce783097c2d
Le Tuan Anh <tuananh.ke@gmail.com>
2018-08-05 13:55:29 +0800

    Move ptpos_to_wn() to lelesk

--- coolisf/common.py
+++ coolisf/common.py
@@ -40,6 +40,7 @@ import gzip
 import logging
 
 from chirptext import FileHelper
+from lelesk.util import ptpos_to_wn
 
 
 # ----------------------------------------------------------------------
@@ -96,65 +97,6 @@ def tags_to_concepts(sent):
         if tokens:
             sent.new_concept(tag.label, tokens=tokens)
     return sent
-
-
-def ptpos_to_wn(ptpos):
-    ''' Penn Treebank Project POS to WN '''
-    # Ref: http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
-    # CC Coordinating conjunction
-    # CD Cardinal number
-    # DT Determiner
-    # EX Existential there
-    # FW Foreign word
-    # IN Preposition or subordinating conjunction
-    # -----------------------------
-    # JJ Adjective
-    # JJR Adjective, comparative
-    # JJS Adjective, superlative
-    # -----------------------------
-    # LS List item marker
-    # MD Modal
-    # -----------------------------
-    # NN Noun, singular or mass
-    # NNS Noun, plural
-    # NNP Proper noun, singular
-    # NNPS Proper noun, plural
-    # -----------------------------
-    # PDT Predeterminer
-    # POS Possessive ending
-    # PRP Personal pronoun
-    # PRP$ Possessive pronoun
-    # -----------------------------
-    # RB Adverb
-    # RBR Adverb, comparative
-    # RBS Adverb, superlative
-    # -----------------------------
-    # RP Particle
-    # SYM Symbol
-    # TO to
-    # UH Interjection
-    # -----------------------------
-    # VB Verb, base form
-    # VBD Verb, past tense
-    # VBG Verb, gerund or present participle
-    # VBN Verb, past participle
-    # VBP Verb, non­3rd person singular present
-    # VBZ Verb, 3rd person singular present
-    # -----------------------------
-    # WDT Wh­determiner
-    # WP Wh­pronoun
-    # WP$ Possessive wh­pronoun
-    # WRB Wh­adverb
-    if ptpos.startswith('JJ'):
-        return 'a'
-    elif ptpos.startswith('NN'):
-        return 'n'
-    elif ptpos.startswith('RB'):
-        return 'r'
-    elif ptpos.startswith('VB'):
-        return 'v'
-    else:
-        return 'x'
 
 
 def get_ep_lemma(ep):
