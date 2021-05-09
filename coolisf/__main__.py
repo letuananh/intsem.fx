6eb3d3d713f2d65bd630ae37cfb00e9abe7e6df9
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-09 21:39:40 +0800

    rename djangoisf to coolisf.rest

--- coolisf/__main__.py
+++ coolisf/__main__.py
@@ -1,2 +1,6 @@
+# This code is a part of coolisf library: https://github.com/letuananh/intsem.fx
+# :copyright: (c) 2014 Le Tuan Anh <tuananh.ke@gmail.com>
+# :license: MIT, see LICENSE for more details.
+
 from . import main
 main.main()
